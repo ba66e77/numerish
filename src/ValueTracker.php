f3a022bff0bf924f75e5fae49a17de50e1173951
Barrett Smith <barrett@datasmith.net>
2016-03-13 10:50:11 -0400

    Fixing description attribute typo, removing int and string type hinting, and catching error at __set method.

--- src/ValueTracker.php
+++ src/ValueTracker.php
@@ -13,26 +13,28 @@ class ValueTracker {
 
     protected $value;
     protected $title;
-    protected $descripion;
+    protected $description;
     protected $moreInfoURL;
 
     /**
      * Constructor function.
      *
      */
-    public function __construct(int $value, string $title = NULL, string $description = NULL, string $moreInfoURL = NULL) {
+    public function __construct($value, $title = null, $description = null, $moreInfoURL = null) {
         $this->value = $value;
         $this->title = $title;
-        $this->descriptoin = $description;
+        $this->description = $description;
         $this->moreInfoURL = $moreInfoURL;
     }
 
-    public function __set(sting $name, mixed $value) {
+    public function __set($name, $value) {
         try {
             $this->validateParameter($name, $value);
             $this->$name = $value;
         }
-
+        catch (Exception $e) {
+            throw new Exception("Unable to set $name");
+        }
     }
 
     /**
@@ -40,17 +42,27 @@ class ValueTracker {
      *
      * @parameter $attributeName;
      * @parameter $value;
+     *
+     * @return true
      */
-    protected function validateParameter(string $attributeName, $value) {
+    protected function validateParameter($attributeName, $value) {
         switch ($attributeName) {
             case 'value':
+                throw new \Exception("No validator for $attributeName");
+                break;
             case 'title':
+                throw new \Exception("No validator for $attributeName");
+                break;
             case 'description':
+                throw new \Exception("No validator for $attributeName");
+                break;
             case 'moreInfoURL':
+                throw new \Exception("No validator for $attributeName");
+                break;
             default:
-              throw new UnexpectedValueException("Unexpected attributeName supplied or supplied attribute does not have defined validation. Got $attributeName.")
+                throw new \UnexpectedValueException("Unexpected attributeName supplied. Got $attributeName.");
         }
-
+        return true;
     }
 }
 
